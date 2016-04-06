b5a10b3100305ba5a58859c689bf6e1a93a265e7
MrLokans <trikster1911@gmail.com>
2016-04-06 16:44:54 +0300

    Crawler now uses items to generate result.

--- onliner_spider.py
+++ onliner_spider.py
@@ -1,8 +1,10 @@
 import scrapy
 
-import io
-import re
-import itertools
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+# from scrapy.selector import Selector
+
+from items import PostItem
 
 BASE_URL = "http://baraholka.onliner.by"
 BOOK_FORUM = "viewforum.php?f=203"
@@ -14,15 +16,20 @@ PAGE_2_URL = START_URL + "&sk=created&start=50"
 # Here for simplicity sake only 20 pages are being parsed.
 # The better way is to obtain pagination widget and count the number
 # of total post pages.
+PAGES_COUNT = 5
 PAGES_URLS = [
-    PAGE_NUMBER_URL.format(x * 50) for x in range(2, 20)
+    PAGE_NUMBER_URL.format(x * 50) for x in range(2, PAGES_COUNT)
 ]
 
 
-class BookSpider(scrapy.Spider):
-    name = "speechspider"
+class BookSpider(CrawlSpider):
+    name = "onlinerbooksspider"
     start_urls = [START_URL]
     start_urls.extend(PAGES_URLS)
+
+    # rules = [Rule(
+    #     LinkExtractor(restrict_css="h2.wraptxt a::attr('href')"),
+    #     callback='parse_topic_page')]
 
     def parse(self, response):
         topic_url_selector = "h2.wraptxt a::attr('href')"
@@ -38,11 +45,12 @@ class BookSpider(scrapy.Spider):
         content_selector = "div.content"
         img_selector = "img.msgpost-img::attr('src')"
 
-        result = {
-            "title": response.css(header_title_selector).extract()[0],
-            "author_nickname": response.css(author_nickname_selector).extract()[0],
-            "content_text": response.css(content_selector).extract()[0],
-            "images": [img for img in response.css(img_selector).extract()]
-        }
+        post = PostItem()
+        post["title"] = response.css(header_title_selector).extract()
+        post["author"] = response.css(author_nickname_selector).extract()
+        post["content"] = response.css(content_selector).extract()
+        post["images"] = []
+        for img in response.css(img_selector).extract():
+            post["images"].append(img)
 
-        yield result
+        yield post
