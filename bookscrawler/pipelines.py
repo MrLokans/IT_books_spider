f5094bb0e0204b1fe60563d0e7fd1d6b5b3de6a5
MrLokans <trikster1911@gmail.com>
2016-04-08 21:01:48 +0300

    Crawler now uses mongodb as its backend.

--- bookscrawler/pipelines.py
+++ bookscrawler/pipelines.py
@@ -4,6 +4,39 @@
 import json
 import io
 
+import pymongo
+
+from scrapy.exceptions import DropItem
+from scrapy import log
+from scrapy.conf import settings
+
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item, spider):
+        if item["title"] and item["author"] and item["content"]:
+            search_criteria = {
+                'title': item['title'],
+                'content': item['content'],
+                'author': item['content'],
+            }
+            mongo_item = self.collection.find_one(search_criteria)
+            if mongo_item:
+                raise DropItem("Item already present.")
+            else:
+                self.collection.insert(dict(item))
+                log.msg("Question added to MongoDB database!",
+                        level=log.DEBUG, spider=spider)
+        return item
+
 
 class JsonWithEncodingPipeline(object):
 
--- bookscrawler/settings.py
+++ bookscrawler/settings.py
@@ -3,4 +3,10 @@ BOT_NAME = 'onlinerbooksspider'
 SPIDER_MODULES = ['bookscrawler.spiders']
 NEWSPIDER_MODULE = 'bookscrawler.spiders'
 
-ITEM_PIPELINES = ['bookscrawler.pipelines.JsonWithEncodingPipeline']
+ITEM_PIPELINES = ['bookscrawler.pipelines.JsonWithEncodingPipeline',
+                  'bookscrawler.pipelines.MongoDBPipeline']
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "onliner_books"
+MONGODB_COLLECTION = "books"
