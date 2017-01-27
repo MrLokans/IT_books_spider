bdd400d593a7b71114eab0ce8ac61112cc1478ef
MrLokans <trikster1911@gmail.com>
2017-01-27 22:37:43 +0300

    Virtualenv installation added.

--- fabfile.py
+++ fabfile.py
@@ -22,6 +22,9 @@ config = ConfigParser.ConfigParser()
 REPOSITORY_URL = 'https://github.com/MrLokans/IT_books_spider'
 PROJECT_DIR = '/opt/IT_books_spider'
 VIRTUALENV_DIR = '/opt/it_books_venv'
+VIRTUALENV_PYTHON = os.path.join(VIRTUALENV_DIR, 'bin/python3')
+VIRTUALENV_PIP = os.path.join(VIRTUALENV_DIR, 'bin/pip')
+REQUIREMENTS_PATH = os.path.join(PROJECT_DIR, 'requirements.txt')
 CONFIG_FILE = os.path.abspath('config.ini')
 
 
@@ -49,10 +52,15 @@ def checkout_repository():
 
 def create_virtualenv():
     logger.info("Creating virtual environemnt.")
+    sudo('python3 -m virtualenv {path}'
+         .format(path=VIRTUALENV_DIR))
 
 
 def install_dependencies():
     logger.info("Installing depenencies")
+    sudo('{pip} install -r {deps_file}'
+        .format(pip=VIRTUALENV_PIP,
+                deps_file=REQUIREMENTS_PATH))
 
 
 def add_crontab_entry():
@@ -68,4 +76,4 @@ def deploy():
     checkout_repository()
     create_virtualenv()
     install_dependencies()
-    add_crontab_entry()
+    # add_crontab_entry()
